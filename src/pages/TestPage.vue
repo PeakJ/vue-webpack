87e3ac91a05f6076ffa3b7288e7237bab83ca4c6
jiangfeng <jiangfeng@yongche.com>
2018-12-01 13:41:20 +0800

    init repo

---
+++ src/pages/TestPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>test页面</div>
+    <div>data:{{testValue}}</div>
+    <div class="my-div"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TestPage',
+  data () {
+    return {
+      testValue: 'bryce'
+    }
+  }
+}
+</script>
+<style scoped>
+  .my-div{
+    background: red;
+    width: 200px;
+    height: 88px;
+    margin: 0 auto
+  }
+</style>
